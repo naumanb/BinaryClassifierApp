2699290aa0ea3a9381491e11fe20fecac9975f14
Nauman Baig <naumanmbaig@gmail.com>
2024-01-04 02:30:48 +0000

    three classifiers added

--- app.py
+++ app.py
@@ -23,6 +23,7 @@ from sklearn.preprocessing import LabelEncoder
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import ConfusionMatrixDisplay, RocCurveDisplay, PrecisionRecallDisplay
 from sklearn.metrics import precision_score, recall_score
+import matplotlib.pyplot as plt
 
 LOGGER = get_logger(__name__)
 
@@ -58,17 +59,17 @@ def main():
     def plot_metrics(metrics_list):
         if 'Confusion Matrix' in metrics_list:
             st.subheader("Confusion Matrix")
-            ConfusionMatrixDisplay(model,x_test, y_test, display_labels=class_names)
+            ConfusionMatrixDisplay.from_estimator(model,x_test, y_test, display_labels=class_names)
             st.pyplot()
 
         if 'ROC Curve' in metrics_list:
             st.subheader("ROC Curve")
-            RocCurveDisplay(model,x_test, y_test)
+            RocCurveDisplay.from_estimator(model,x_test, y_test)
             st.pyplot()
 
-        if "Precision-Recall Curve" in metrics_list:
+        if 'Precision-Recall Curve' in metrics_list:
             st.subheader("Precision-Recall Curve")
-            PrecisionRecallDisplay(model, x_test, y_test)
+            PrecisionRecallDisplay.from_estimator(model, x_test, y_test)
             st.pyplot()
 
     df = load_data()
@@ -78,15 +79,57 @@ def main():
 
 
     if classifier == 'Support Vector Machine (SVM)':
-        C = st.sidebar.number_input("C (Regularization Parameter)", 0.01, 10.0, step=0.01, key='C')
+        C = st.sidebar.number_input("C (Regularization Parameter)", 0.01, 10.0, step=0.01, key='C_SVM')
         kernel = st.sidebar.radio("Kernel",("rbf", "linear"), key='kernel')
         gamma = st.sidebar.radio("Kernel Coefficient", ("scale", "auto"), key="gamma")
 
-        metrics = st.sidebar.multiselect("What metrics to plot?", ('Confusion Matrix', 'ROC Curve', 'Precision Recall '))
+        metrics = st.sidebar.multiselect("What metrics to plot?", ('Confusion Matrix', 'ROC Curve', 'Precision-Recall Curve'))
 
+        if st.sidebar.button("Classify", key="classify"):
+            st.subheader("Support Vector Machine (SVM) Results")
+            model = SVC(C=C, kernel=kernel, gamma=gamma)
+            model.fit(x_train, y_train)
+            accuracy = model.score(x_test, y_test)
+            y_pred = model.predict(x_test)
+            st.write("Accuracy: ", accuracy.round(2))
+            st.write("Precision: ", precision_score(y_test, y_pred, labels=class_names).round(2))
+            st.write("Recall: ", recall_score(y_test, y_pred, labels=class_names).round(2))
+            plot_metrics(metrics)
 
+    if classifier == 'Logistic Regression':
+        C = st.sidebar.number_input("C (Regularization Parameter)", 0.01, 10.0, step=0.01, key='C_LR')
+        max_iter = st.sidebar.slider("Maximum # of iterations", 100, 500, key="max_iter")
 
+        metrics = st.sidebar.multiselect("What metrics to plot?", ('Confusion Matrix', 'ROC Curve', 'Precision-Recall Curve'))
 
+        if st.sidebar.button("Classify", key="classify"):
+            st.subheader("Logistic Regression Results")
+            model = LogisticRegression(C=C, max_iter=max_iter)
+            model.fit(x_train, y_train)
+            accuracy = model.score(x_test, y_test)
+            y_pred = model.predict(x_test)
+            st.write("Accuracy: ", accuracy.round(2))
+            st.write("Precision: ", precision_score(y_test, y_pred, labels=class_names).round(2))
+            st.write("Recall: ", recall_score(y_test, y_pred, labels=class_names).round(2))
+            plot_metrics(metrics)
+
+    if classifier == 'Random Forest':
+        n_trees = st.sidebar.number_input("The # of trees in the forest", 100, 5000, step=10, key='n_trees')
+        max_depth = st.sidebar.number_input("Max depth of the tree", 1, 20, step=1, key='max_depth')
+        bootstrap = st.sidebar.radio("Bootstrap samples when building trees", (True, False), key='bootstrap')
+
+        metrics = st.sidebar.multiselect("What metrics to plot?", ('Confusion Matrix', 'ROC Curve', 'Precision-Recall Curve'))
+
+        if st.sidebar.button("Classify", key="classify"):
+            st.subheader("Random Forest Results")
+            model = RandomForestClassifier(n_estimators=n_trees, max_depth=max_depth, bootstrap=bootstrap, n_jobs=-1)
+            model.fit(x_train, y_train)
+            accuracy = model.score(x_test, y_test)
+            y_pred = model.predict(x_test)
+            st.write("Accuracy: ", accuracy.round(2))
+            st.write("Precision: ", precision_score(y_test, y_pred, labels=class_names).round(2))
+            st.write("Recall: ", recall_score(y_test, y_pred, labels=class_names).round(2))
+            plot_metrics(metrics)
 
 
     if st.sidebar.checkbox("Show raw data", False):
